65df2523ba0a6a2d0b0a7b46283d0fa715530da4
Siddika-akhter <siddika.tas28@gmail.com>
2020-07-17 22:38:24 +0600

    some edit end

--- assignment.js
+++ assignment.js
@@ -26,14 +26,14 @@ console.log(total)
 // tinyFriend
 
 function smallNumber(name){
-    var small = name;
+    var smallest = name;
     for (var i = 0; i < name.length; i++){
         var currentName = name[i];
-        if(currentName < small){
-            small = currentName;
+        if(currentName < smallest){
+            smallest = currentName;
         }
     }
-    return small;
+    return smallest;
 }
 var smallFriend = smallNumber(['Iv','nipunReza','nazmul','babu','siddika','tasmim']);
 console.log(smallFriend);
@@ -42,18 +42,18 @@ console.log(smallFriend);
 
 // brickCalculator
 
-function brickCalculator(floor1, floor2, floor3){
-   var sum1 = floor1*15*1000;
+function brickCalculator(firstheight, middleheight, lastheight){
+   var brickTotal1 = firstheight*15*1000;
    
-   var sum2 = floor2*12*1000;
+   var brickTotal2  = middleheight*12*1000;
    
-   var sum3 = floor3*10*1000;
+   var brickTotal3 = lastheight*10*1000;
   
-   var total = sum1+sum2+sum3
+   var total = brickTotal1 + brickTotal2 + brickTotal3;
     
     return total;
 }
-var result = brickCalculator(10,10,40)
+var result = brickCalculator(10,10,5)
 console.log(result);
 
 
